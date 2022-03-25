9b374a6756a8d16163bb5fade9396317ddb3e65b
Giovanni <g.depunzio@neotech.srl>
2022-03-25 19:32:25 +0100

    prova

--- Libreria01/src/main/java/srl/neotech/academy/StartApplication.java
+++ Libreria01/src/main/java/srl/neotech/academy/StartApplication.java
@@ -1,27 +1,26 @@
 package srl.neotech.academy;
 
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 import org.ajbrown.namemachine.NameGenerator;
 import org.apache.commons.text.RandomStringGenerator;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-
 public class StartApplication extends Biblioteca {
 
 	public static void main(String[] args) {
-    
-			
-	String convertitoInJason = null;	
-	try {
-    ArrayList<Libro> listaLibri= new ArrayList<Libro>();
-    ArrayList <Associato> associati = new ArrayList<Associato>(); 
+
+		
+		ArrayList<Libro> listaLibri= new ArrayList<Libro>();
+        ArrayList <Associato> associati = new ArrayList<Associato>(); 
 	
-		 Biblioteca biblioteca = new Biblioteca();
+			Biblioteca biblioteca = new Biblioteca();	
+	
+		String convertitoInJason = null;	
+	
+    
+		
+		 
 
 		 RandomStringGenerator generator = new RandomStringGenerator
 	                .Builder()
@@ -30,7 +29,9 @@ public class StartApplication extends Biblioteca {
 		  
 		 NameGenerator nameGenerator = new NameGenerator();
 		 
-	  
+	
+			 
+//aggiunta di un libro nella lista libri	CL  
 	
 	for (int i=0;i<=100;i++) {
 		Libro libro = new Libro();
@@ -41,13 +42,22 @@ public class StartApplication extends Biblioteca {
 		libro.setScaffale(generator.generate(5));
 	  	biblioteca.getListaLibri().add(libro);
 	
+	
+	}	
+// aggiunta di un associato nell lista associati	IA  	
+	  	
 	for (int j=0;j<=100;j++ ) {
 		Associato associato = new Associato();
 		associato.setNominativo(generator.generate(15));
 		associato.setIdAssociato(j);
 		associato.setStato(Stato.ATTIVO);
-		associato.setIdLibriPresi(null);
+		associato.setIdLibriPresi(associato.getIdLibriPresi());
 		biblioteca.getListaAssociati().add(associato);
+		
+		// rimozione di un libro dall lista libri XL
+		
+	//	Libro libro1 = new Libro();
+	//	biblioteca.getListaLibri().get(i).getIdLibro();
 		
 		
 		
@@ -56,23 +66,28 @@ public class StartApplication extends Biblioteca {
 //		} catch (JsonProcessingException e) {
 //			e.printStackTrace();
 //		}
-	//	System.out.println(convertitoInJason);
+//		System.out.println(convertitoInJason);
+//	
 	
 	
-		String scelta = null;
+	}
+	
+	String controllo = "XX";
+	String scelta = null;
 		
+	do {
 		
 		Scanner scan= new Scanner(System.in);
-		System.out.println("Lista di possibili comandi che l'utente puo eseguire:"
+		System.out.println("Lista di possibili comandi che l'utente puo eseguire:\n"
 //			
-			+ "CL	classificareLibro  -->aggiunta di un libro alla lista dei libri della Biblioteca\n"
-			+ "XL	rimuovereLibro     -->rimozione di un libro dalla lista dai libri della Biblioteca\n"
-			+ "LL   listaLibri         -->visualizzare la lista dei libri e per ogni libro stampare le info\n"
-			+ "IA	iscrivereAssociato -->aggiunta di un associato alla lista degli Associati\n"
-			+ "XA	cancellareAssociato-->rimozione di un associato dalla lista degli Associati\n"
-			+ "LA   listaAssociati     -->visualizzare la lista degli associati e per ogni associato stampare le info (compresa la lista dei libriPresi)\n"
-			+ "PL	presaLibro         -->aggiunta alla lista deli libri dell'associato\n"
-			+ "RL	restituzioneLibro  -->rimozione dalla lista deli libri dell'associato\n");
+			+ "CL	Classificare	Libro		-->aggiunta di un libro alla lista dei libri della Biblioteca\n"
+			+ "XL	Rimuovere	Libro		-->rimozione di un libro dalla lista dai libri della Biblioteca\n"
+			+ "LL	Lista		Libri		-->visualizzare la lista dei libri e per ogni libro stampare le info\n"
+			+ "IA	Iscrivere	Associato	-->aggiunta di un associato alla lista degli Associati\n"
+			+ "XA	Cancellare	Associato	-->rimozione di un associato dalla lista degli Associati\n"
+			+ "LA	Lista		Associati	-->visualizzare la lista degli associati e per ogni associato stampare le info (compresa la lista dei libriPresi)\n"
+			+ "PL	Presa		Libro		-->aggiunta alla lista deli libri dell'associato\n"
+			+ "RL	Restituzione	Libro		-->rimozione dalla lista deli libri dell'associato\n");
 //	
 		System.out.println("Prego inserire istruzione che si vuole eseguire tra quelle sopra elencate:");
 		String inserireComando= scan.next();
@@ -81,41 +96,95 @@ public class StartApplication extends Biblioteca {
 			switch (inserireComando) { 
 //     		
      		case "CL": 
+     			
      			scelta = "CL classificare Libro"; 			
-     			System.out.println("scelta");
-     		break; 
+     			Libro libro= new Libro();
+     			libro.setIdLibro(biblioteca.getListaLibri().size()+1);
+     			Scanner titolo= new Scanner(System.in);
+     			System.out.println("Quale titolo vuoi aggiungere?");
+     			String titoloDaAggiungere = titolo.next();
+     			Scanner autore= new Scanner(System.in);
+     			System.out.println("Quale autore vuoi aggiungere?");
+     			String autoreDaAggiungere = autore.next();
+     			Scanner genere= new Scanner(System.in);
+     			System.out.println("Quale genere vuoi aggiungere?");
+     			String genereDaAggiungere = genere.next();
+     			libro.setScaffale(generator.generate(5));
+     			biblioteca.getListaLibri().add(libro);
+     			controllo ="CL";
+   				System.out.println("E' stato aggiunto un libro:");
+     			break; 
+    
      		case  "XL":
-     			scelta= "XL Rimuovere Libro";
-     			System.out.println("scelta");
-     		break; 
-     		case "LL listaLibri":  
+     			scelta= "XL E' stato rimosso un Libro";
+     			controllo = "XL";
+     			System.out.println("Quale titolo di libro desideri rimuoveve?");
+     			Scanner libroRim = new Scanner(System.in);
+     			String libroDaRimuovere = libroRim.next();
+     			
+     			
+     			
+     			
+     			System.out.println(scelta);
+     			break; 
+    
+     		case "LL":  
      			scelta= "LL Lista Libri";
-     			System.out.println("scelta");
-     			break; 
-     		case  "IA IscrivereAssociato": 
-     			System.out.println("scelta");
-     			break;
-     		case "XA CancellareAssociato": 
-     			System.out.println("scelta");
-     			break; 
-     		case "LA ListaAssociati": 
-     			System.out.println("scelta");
-     			break;
-     		case  "PL PresaLibro": 
-     			System.out.println("scelta");
-     			break; 
-     		case "RL RestituzioneLibro": 
-     			System.out.println("scelta");
-     			break;
      		
-     		
+     			
+    			Integer lunghezzaArrayLibri=biblioteca.getListaLibri().size();
+    			controllo="LL";
+    		for(int i=0;i<lunghezzaArrayLibri;i++) {
+    			if((int) (biblioteca.getListaLibri().get(i).getIdLibro())==0){
+    			}else {
+    			libro=biblioteca.getListaLibri().get(i);
+    			System.out.println(libro);
+    			}
+    			}
+    			break;
+     		case  "IA": 
+     			scelta= "IA E' stato iscritto un nuovo Associato";
+     			System.out.println(scelta);
+     			break;
+     		case "XA": 
+     			scelta= "XA E' stato cancellato un Associato"; 
+     			System.out.println(scelta);
+     			break; 
+     		case "LA": 
+     			scelta = "LA Lista Associati";
+     			System.out.println(scelta);
+     			break;
+     		case  "PL": 
+     			scelta ="PL E' stato preso Presa un Libro";
+     			System.out.println(scelta);
+     			break; 
+     		case "RL": 
+     			scelta= "RL E' stato restituito un Libro";
+     			System.out.println(scelta);
+     			break;
+     		case "XX": 
+     			scelta= "XX Uscita dal Programma";
+     			System.out.println(scelta);
+     			break;
+
+     			
+     			
+     			
 			}
-	}
+		
+     			
+     			
+			}  while(!(controllo.equals("XX")));
+			
+			System.out.println("[PROGRAMMA TERMINATO]");
+     			
+	}	
+     			
+			}
+
 	
 	
-	
-	
-	
+
 	
 	
 	
--- Libreria01/src/main/java/srl/neotech/academy/Stato.java
+++ Libreria01/src/main/java/srl/neotech/academy/Stato.java
@@ -8,13 +8,13 @@ public enum Stato {
 	SOSPESO,
 	BANDITO;
 	
-	public static Stato getRandom() {
-        return values()[(int) (Math.random() * values().length)];
-    }
-
-    public static void main(String[] args) {
-        System.out.println(Stato.getRandom());
-    }
+//	public static Stato getRandom() {
+//        return values()[(int) (Math.random() * values().length)];
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(Stato.getRandom());
+//    }
 }
 	
 	
