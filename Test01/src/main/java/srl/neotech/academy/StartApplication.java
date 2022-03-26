8ada55a390e6047ff042145612fa993b339f0eb5
Giovanni <g.depunzio@neotech.srl>
2022-03-26 10:29:05 +0100

    Aggiorno

--- Test01/src/main/java/srl/neotech/academy/StartApplication.java
+++ Test01/src/main/java/srl/neotech/academy/StartApplication.java
@@ -1,10 +1,14 @@
 package srl.neotech.academy;
 
+import org.kohsuke.randname.RandomNameGenerator;
+
 public class StartApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		RandomNameGenerator rnd = new RandomNameGenerator(0);
+		
 	}
 
 }
